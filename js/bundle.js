66aa4f74ce9c9d5bfdcdcee0d235fd48e32e4144
AntonOshurek <83346452+AntonOshurek@users.noreply.github.com>
2022-04-18 15:18:27 +0000

    Deploying to gh-pages from @ AntonOshurek/collagen@4d91bd94e6e6769627550bcc876d72f88dad80b2 🚀

---
+++ js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************************!*\
+  !*** ./source/scripts/index.js ***!
+  \*********************************/
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('done');
+});
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
