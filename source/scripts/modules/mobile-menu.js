66f70f253531be417fe7f62cd57739cd844f10df
AntonOshurek <oshurekweb@gmail.com>
2022-04-18 20:27:54 +0200

    refactoring nav btn svg icon

--- source/scripts/modules/mobile-menu.js
+++ source/scripts/modules/mobile-menu.js
@@ -2,18 +2,21 @@ const mobileMenu = () => {
 	const siteBody = document.querySelector('.body');
 	const navButton = document.querySelector('.nav__button');
 	const navList = document.querySelector('.nav__list');
+	const navIcon = document.querySelector('.btn-icon');
 
 	let navigationIsOpen = false;
 
 	const closeNavigation = () => {
 		navigationIsOpen = false;
 		navList.classList.remove('nav__list--open');
+		navIcon.classList.remove('btn-icon--active');
 		siteBody.classList.remove('body--scrolloff');
 	}
 
 	const openNavigation = (evt) => {
 		navigationIsOpen = true;
 		navList.classList.add('nav__list--open');
+		navIcon.classList.add('btn-icon--active');
 		siteBody.classList.add('body--scrolloff');
 
 		window.addEventListener('resize', (e) => {
