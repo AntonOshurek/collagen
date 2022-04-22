f41b9a3f9730abf15e4744938e1d7cf4c1398afb
AntonOshurek <oshurekweb@gmail.com>
2022-04-22 22:24:04 +0200

    add adaptive for product page

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,6 +3,13 @@ import { replaceCartIcon } from './modules/cart-icon';
 
 window.addEventListener('DOMContentLoaded', () => {
   mobileMenu();
-	replaceCartIcon();
+
+	if(document.location.pathname.indexOf('product') > -1) {
+		replaceCartIcon();
+	}
+
+	// if(document.location.pathname === '/projects.html' || document.location.pathname === '/unicorn__arhitekte/projects.html') {
+	//
+	// }
 });
 
