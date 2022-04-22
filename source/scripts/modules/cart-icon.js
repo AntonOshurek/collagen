904cd6796f16268930aa30e7b52005e0e5aee8f2
AntonOshurek <oshurekweb@gmail.com>
2022-04-22 23:00:52 +0200

    add styles for add form

--- source/scripts/modules/cart-icon.js
+++ source/scripts/modules/cart-icon.js
@@ -1,15 +1,15 @@
 const replaceCartIcon = () => {
-	const cartButton = document.querySelector('.product__button');
+	const cartButton = document.querySelector('.add-form__button');
 	let productInCart = true;
 
 	const cartButtonUpdate = () => {
 		if(productInCart) {
 			productInCart = false;
-			cartButton.classList.remove('product__button--remove-from-cart');
+			cartButton.classList.remove('add-form__button--remove-from-cart');
 			cartButton.textContent = 'Добавить в корзину';
 		} else {
 			productInCart = true;
-			cartButton.classList.add('product__button--remove-from-cart');
+			cartButton.classList.add('add-form__button--remove-from-cart');
 			cartButton.textContent = 'Удалить из корзины';
 		}
 	}
