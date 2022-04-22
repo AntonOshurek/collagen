8fc550df464b0bd8b1931cd702fb11e50a638f62
AntonOshurek <oshurekweb@gmail.com>
2022-04-22 22:36:20 +0200

    correction cart button logick

--- source/scripts/modules/cart-icon.js
+++ source/scripts/modules/cart-icon.js
@@ -1,19 +1,30 @@
 const replaceCartIcon = () => {
 	const cartButton = document.querySelector('.product__button');
-	let productInCart = false;
+	let productInCart = true;
 
-	function replaceIcon(evt) {
-		evt.preventDefault();
+	const cartButtonUpdate = () => {
 		if(productInCart) {
 			productInCart = false;
-			this.classList.remove('product__button--remove-from-cart');
+			cartButton.classList.remove('product__button--remove-from-cart');
+			cartButton.textContent = 'Добавить в корзину';
 		} else {
 			productInCart = true;
-			this.classList.add('product__button--remove-from-cart');
+			cartButton.classList.add('product__button--remove-from-cart');
+			cartButton.textContent = 'Удалить из корзины';
 		}
 	}
 
-	cartButton.addEventListener('click', replaceIcon)
+	const checkProductStatus = () => {
+		cartButtonUpdate();
+	}
+
+	function shopingCurtButtonHandler(evt) {
+		evt.preventDefault();
+		cartButtonUpdate();
+	}
+
+	cartButton.addEventListener('click', shopingCurtButtonHandler);
+	checkProductStatus(); // for first launch
 }
 
 export { replaceCartIcon };
